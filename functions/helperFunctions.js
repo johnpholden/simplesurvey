7f44ddf54b24fea52eba2f6af72b0f3a4eb3bee2
johnpholden <46760962+johnpholden@users.noreply.github.com>
2021-11-27 17:47:48 -0700

    Add files via upload

--- functions/helperFunctions.js
+++ functions/helperFunctions.js
@@ -3,30 +3,43 @@ export const sampleSurvey = 'What is the best pizza topping?\n[] Pepperoni\n[] C
 export async function parseSurvey(surveyText) {
     let formattedQuestions = []
   
-    let rawQuestions = surveyText.split('\n\n')
-  
-    rawQuestions.map((q, i) => {
-      let qPrompt = q.match(/^(.*\?$)/gim, '$1')
+    let rawQuestions = '';
+    
+    if(surveyText) {
+    
+        rawQuestions = surveyText.split('\n\n');
       
-      if(qPrompt) {
-        formattedQuestions.push({ prompt: qPrompt[0] })
-      } else {
-        formattedQuestions.push({})
-      }
+        rawQuestions.map((q, i) => {
+          let qPrompt = q.match(/^(.*\?$)/gim, '$1')
+          
+          if(qPrompt) {
+            formattedQuestions.push({ prompt: qPrompt[0] })
+          } else {
+            formattedQuestions.push({})
+          }
+          
+          let mcAnswers = q.match(/^.*\[\*?\].*$/gim)
+          
+          if(mcAnswers != null) {
+            let question = q.split('\n');
+            question[0] = question[0].replace(' ','_');
+
+            mcAnswers = mcAnswers.map((a) => a.replace(/^\[\] (.*$)/gim, '$1'))
+
+            formattedQuestions[i]['mcAnswers'] = mcAnswers
+          }
+          
+          let sAnswers = q.match(/^.*\_\_\_*$/gim)
+          if(sAnswers != null) {
+            sAnswers = sAnswers.map((a) => a.replace(/\_\_\_/gim, ''))
+            formattedQuestions[i]['sAnswers'] = sAnswers
+          }
       
-      let mcAnswers = q.match(/^.*\[\].*$/gim)
-      if(mcAnswers != null) {
-        mcAnswers = mcAnswers.map((a) => a.replace(/^\[\] (.*$)/gim, '$1'))
-        formattedQuestions[i]['mcAnswers'] = mcAnswers
+        })
       }
-      let sAnswers = q.match(/^.*\_\_\_ .*$/gim)
-      if(sAnswers != null) {
-        sAnswers = sAnswers.map((a) => a.replace(/^\_\_\_ (.*$)/gim, '$1'))
-        formattedQuestions[i]['sAnswers'] = sAnswers
-      }
-  
-    })
-  
+ 
+
+ 
     return formattedQuestions
   }
   
