6e3af7a62389ff1ecea0f540d8a0045b03d1b4f0
johnpholden <46760962+johnpholden@users.noreply.github.com>
2021-11-27 17:48:15 -0700

    Add files via upload

--- components/Survey.tsx
+++ components/Survey.tsx
@@ -10,15 +10,17 @@ export default function Survey({ items }: { items: any }) {
           {item.mcAnswers &&
             item.mcAnswers.map((a,y) => (
               <div>
-              <input type="radio" id={y} name={a} value={a}></input>
+              <input type="radio" id={y} name={y} value={a}></input>
               <label for={y}>{a}</label>
               </div>
             ))}
 
           {item.sAnswers &&
-            item.sAnswers.map((a) => (
-              <input placeholder={a}/>
-                
+            item.sAnswers.map((a,y) => (
+              <div>
+              <label for={y}>{a}</label>
+              <input type="text" id={y} name={a}></input>
+              </div>
             ))}
         </form>
       ))}
